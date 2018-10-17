6779a8efb77d499894c3d1892af25e9d51237501
Aleksandra Półtorak <xmacabrax@gmail.com>
2018-10-17 22:04:45 +0200

    Updates

---
+++ precache-manifest.584eb4586e372a958a9c1dc7bc0fdee8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gallery/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "6a638fedae60f6c8df15",
+    "url": "/react-gallery/static/js/runtime~main.6a638fed.js"
+  },
+  {
+    "revision": "46201f59d01bc2adcd28",
+    "url": "/react-gallery/static/js/main.46201f59.chunk.js"
+  },
+  {
+    "revision": "4fd91c74e51506835aa9",
+    "url": "/react-gallery/static/js/1.4fd91c74.chunk.js"
+  },
+  {
+    "revision": "46201f59d01bc2adcd28",
+    "url": "/react-gallery/static/css/main.4b83eb99.chunk.css"
+  },
+  {
+    "revision": "7eabb2bba9253b765b8933d6bd59b2c1",
+    "url": "/react-gallery/index.html"
+  }
+];
