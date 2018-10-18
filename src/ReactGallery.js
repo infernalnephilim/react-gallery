d8baec94539c4055d404901fee101e43a656ad3f
Aleksandra Półtorak <xmacabrax@gmail.com>
2018-10-18 22:19:24 +0200

    Dodanie kolejnych projektów

--- src/ReactGallery.js
+++ src/ReactGallery.js
@@ -4,27 +4,59 @@ import './ReactGallery.css';
 const projectsData = [{
     id: 1,
     title: 'Dermina',
+    description: 'Strona internetowa prywatnego gabinetu dermatologicznego.',
     category: 'strony internetowe',
     imageURL: 'img/dermina_featured3.jpg',
     siteURL: 'http://aleksandra-poltorak.pl/portfolio/dermina.html'
 }, {
     id: 2,
     title: 'JingleX',
+    description: 'Strona firmy JingleX.',
     category: 'strony internetowe',
     imageURL: 'img/jinglex_featured3.jpg',
     siteURL: 'http://aleksandra-poltorak.pl/portfolio/jinglex.html'
 }, {
     id: 3,
     title: 'JingleX 2017',
+    description: 'Poprzednia strona firmy JingleX.',
     category: 'strony internetowe',
     imageURL: 'img/jinglex_old_featured.jpg',
     siteURL: 'http://aleksandra-poltorak.pl/portfolio/jinglex2017.html'
 }, {
     id: 4,
     title: 'Lista zakupów',
+    description: 'Aplikacja wykonana w AngularJS',
     category: 'aplikacje',
     imageURL: 'img/lista-zakupow_featured.jpg',
     siteURL: 'http://aleksandra-poltorak.pl/portfolio/lista-zakupow.html'
+}, {
+    id: 5,
+    title: 'Praca licencjacka',
+    description: 'Modelowanie scen 3D z wykorzystaniem PixiJS',
+    category: 'aplikacje',
+    imageURL: 'img/pixijs_featured.jpg',
+    siteURL: 'http://aleksandra-poltorak.pl/portfolio/modelowanie-scen-3d.html'
+}, {
+    id: 6,
+    title: 'Portfolio',
+    description: 'Poprzednia wersja portfolio',
+    category: 'strony internetowe',
+    imageURL: 'img/portfolio_featured.jpg',
+    siteURL: 'http://aleksandra-poltorak.pl/portfolio/portfolio-bootstrap.html'
+}, {
+    id: 7,
+    title: 'Portfolio',
+    description: 'Nowa wersja portfolio',
+    category: 'strony internetowe',
+    imageURL: 'img/portfolio_nowe_featured.jpg',
+    siteURL: 'http://aleksandra-poltorak.pl/portfolio/portfolio-flex.html'
+},{
+    id: 8,
+    title: 'Preloadery',
+    description: 'Zestaw animacji ładowania strony',
+    category: 'dodatki',
+    imageURL: 'img/preloader_featured.jpg',
+    siteURL: 'https://aleksandra-poltorak.pl/portfolio/preloader.html'
 }];
 
 class Item extends Component {
@@ -32,16 +64,18 @@ class Item extends Component {
         return (
             <div className="ReactGallery__item" data-category={this.props.category}>
                 <img src={this.props.imageURL}/>
-                <span className="ReactGallery-item__title">
-                    {this.props.title}
-                </span>
-                <span className="ReactGallery-item__description">
+                <div className="ReactGallery-item__text">
+                    <h4 className="ReactGallery-item__title">
                         {this.props.title}
+                    </h4>
+                    <span className="ReactGallery-item__description">
+                        {this.props.description}
                         </span>
-                <div className="ReactGallery-item__details">
-                    <a className="button" href={this.props.siteURL}>
-                        Szczegóły
-                    </a>
+                    <div className="ReactGallery-item__details">
+                        <a className="button" href={this.props.siteURL}>
+                            Szczegóły
+                        </a>
+                    </div>
                 </div>
             </div>
         )
@@ -110,7 +144,7 @@ class ReactGallery extends Component {
                     {
                         this.state.displayedItems.map(function (el) {
                             return <Item key={el.id} imageURL={el.imageURL} category={el.category} title={el.title}
-                                         siteURL={el.siteURL}/>
+                                         siteURL={el.siteURL} description={el.description}/>
                         })
                     }
                 </div>
